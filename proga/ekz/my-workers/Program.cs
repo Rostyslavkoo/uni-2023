844702b2369d6f9c4cb58ef30f17213da940522a
Rostyslav Urdeichuk <snotor.urdeichuk.r@gmail.com>
2024-06-07 00:47:19 +0300

    ekx

---
+++ proga/ekz/my-workers/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program()
+{
+    public static void Main(string[] args)
+    {
+        Data data = new Data();
+
+        data.Load1("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/input/inputBrigade.xml");
+        data.Load2("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/input/inputWorkerxml.xml");
+        data.Load3("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/input/inputTarrif.xml");
+        data.Load4("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/input/inputItem.xml");
+        var report1 = data.Load5("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/input/inputReport_1.xml");
+        var report2 = data.Load5("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/input/inputReport_2.xml");
+        data.Reports = report1.Concat(report2).ToList();
+        
+
+        data.TaskA("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/output/output1.xml");
+        data.TaskB("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/tarrifs/tarrifs/output/output2.xml");
+        //data.TaskC("output/output3.xml");
+    }
+}
---
+++ proga/xml/results/MyPract/MyPract/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Data data = new Data();
+        data.Load1("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/MyPract/MyPract/input/input1.xml");
+        data.Load2("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/MyPract/MyPract/input/input2.xml");
+        data.Load3("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/MyPract/MyPract/input/input3.xml");
+
+        // data.TaskA("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/MyPract/MyPract/output/output1.xml");
+        // data.TaskB("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/MyPract/MyPract/output/output2.xml");
+        // data.TaskC("/Users/rostislavurdejcuk/My Drive/lnu/uni-2023/proga/xml/results/MyPract/MyPract/output/output3.xml");
+
+
+    }
+}
